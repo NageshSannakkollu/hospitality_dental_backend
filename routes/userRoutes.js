33f029d68be88d212f23b3f6e87574a71fc93dba
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2025-09-03 11:33:22 +0530

    user_routes updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const {userRegistration,loginUser} = require("../controllers/userController")
 //const authenticateToken = require("../middleware/authMiddleware")
 
-
 const router = express.Router()
+
 router.post("/api/auth/signup",userRegistration)
 router.post("/api/auth/login",loginUser)
 
